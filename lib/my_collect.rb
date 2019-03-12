ce811f8b9e37f20bb496b3c31a732526ce754d26
Reinald Reynoso <reireynoso@gmail.com>
2019-03-12 01:43:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(empty_array)
   collection = empty_array
   arr = []
   i = 0
-  while i < collection.length  do |i|
-    arr << i.upcase
+  while i < collection.length  do 
+    
     i += 1
   end
   arr
